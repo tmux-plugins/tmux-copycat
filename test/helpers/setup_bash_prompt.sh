10f650425389f3db8f8b32b23a4c169556832430
Bruno Sutic <bruno.sutic@gmail.com>
2014-07-31 15:22:14 +0200

    Setup PS1 prompt for test VM in provisioning file

--- test/helpers/setup_bash_prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tests helper script for setting up `.tmux.conf` within the VM.
-# To be used by sourcing from within individual test scripts.
-
-setup_bash_prompt() {
-	echo 'export PS1="\w\$"' > ~/.bashrc
-}
-setup_bash_prompt
