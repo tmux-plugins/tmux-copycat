d364efc75d5cc6a39f60c2d6a8f03e67b5d916d6
Bruno Sutic <bruno.sutic@gmail.com>
2014-07-31 16:18:42 +0200

    Setup centos box for tests

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,15 @@
 VAGRANTFILE_API_VERSION = '2'
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = 'precise32'
-  config.vm.box_url = 'http://files.vagrantup.com/precise32.box'
 
-  config.vm.provision 'shell', path: 'vagrant_provisioning.sh'
+  config.vm.define :ubuntu do |ubuntu|
+    ubuntu.vm.box = 'hashicorp/precise32'
+    ubuntu.vm.provision 'shell', path: 'vagrant_ubuntu_provisioning.sh'
+  end
+
+  config.vm.define :centos do |centos|
+    centos.vm.box = 'chef/centos-6.5'
+    centos.vm.provision 'shell', path: 'vagrant_centos_provisioning.sh'
+  end
+
 end
---
+++ vagrant_centos_provisioning.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# override PS1 prompt
+echo 'export PS1="\w \$ "' >> ~/.bashrc
+
+# tmux installation instructions from here
+# https://gist.github.com/rschuman/6168833
+
+sudo su -
+
+yum -y install gcc kernel-devel make ncurses-devel
+yum -y install git-core expect vim
+
+# download tmux and libevent
+mkdir ~/downloads && cd ~/downloads
+curl http://sourceforge.net/projects/levent/files/latest/download?source=files -L -o libevent2.tar.gz -w 'Last URL was: %{url_effective}'
+curl http://sourceforge.net/projects/tmux/files/latest/download?source=files -L -o tmux.tar.gz -w 'Last URL was: %{url_effective}'
+
+# compile libevent
+cd ~/downloads
+tar zxvf libevent2.tar.gz
+cd libevent-*-stable
+./configure --prefix=/usr/local
+make
+make install
+
+# compile tmux
+cd ~/downloads
+tar zxvf tmux.tar.gz
+cd tmux-*
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make
+make install
