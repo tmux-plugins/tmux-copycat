6ebb622f714a237511b8a6ea369a3fc73144bf24
Bruno Sutic <bruno.sutic@gmail.com>
2014-08-02 18:17:59 +0200

    Add tests for custom defined search in `.tmux.conf`

---
+++ test/helpers/setup_tmux_conf_with_custom_searches.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Tests helper script for setting up `.tmux.conf` within the VM.
+# To be used by sourcing from within individual test scripts.
+
+setup_tmux_conf() {
+	# Copy mode (vi or emacs) is automatically determined from EDITOR
+	# environment variable set in test runner file `test/run-tests-within-vm`.
+	echo "bind-key -t vi-copy    y copy-selection"  > ~/.tmux.conf
+	echo "bind-key -t emacs-copy y copy-selection" >> ~/.tmux.conf
+	echo "set -g @copycat_search_C-t 'random string[[:digit:]]\+'" >> ~/.tmux.conf
+	echo "run-shell '/vagrant/copycat.tmux'"       >> ~/.tmux.conf
+}
+setup_tmux_conf
