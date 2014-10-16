3514c20f203523e5081bcabb1662ca9d1c81c740
Bruno Sutic <bruno.sutic@gmail.com>
2014-10-16 22:44:16 +0200

    Fix custom search tests (remove backslash)

--- test/helpers/setup_tmux_conf_with_custom_searches.sh
+++ test/helpers/setup_tmux_conf_with_custom_searches.sh
@@ -8,7 +8,7 @@ setup_tmux_conf() {
 	# environment variable set in test runner file `test/run-tests-within-vm`.
 	echo "bind-key -t vi-copy    y copy-selection"  > ~/.tmux.conf
 	echo "bind-key -t emacs-copy y copy-selection" >> ~/.tmux.conf
-	echo "set -g @copycat_search_C-t 'random string[[:digit:]]\+'" >> ~/.tmux.conf
+	echo "set -g @copycat_search_C-t 'random string[[:digit:]]+'" >> ~/.tmux.conf
 	echo "run-shell '/vagrant/copycat.tmux'"       >> ~/.tmux.conf
 }
 setup_tmux_conf
