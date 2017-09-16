eff2f0f4451f6f6a506f52703ab5516f9dc4df17
Thales Mello <thalesmello@gmail.com>
2017-09-15 22:58:55 -0300

    Include tmux 2.4 in the tests
    The tests are currently failing, as expected because the public
    interface of the API was broken.

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,11 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     ubuntu.vm.provision 'shell', path: 'vagrant_ubuntu_provisioning.sh'
   end
 
+  config.vm.define :ubuntu_two_four do |ubuntu|
+    ubuntu.vm.box = 'hashicorp/precise32'
+    ubuntu.vm.provision 'shell', path: 'vagrant_ubuntu_provisioning_two_four.sh'
+  end
+
   config.vm.define :centos do |centos|
     centos.vm.box = 'chef/centos-6.5'
     centos.vm.provision 'shell', path: 'vagrant_centos_provisioning.sh'
---
+++ vagrant_ubuntu_provisioning_two_four.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# override PS1 prompt
+echo 'export PS1="\$ "' >> /home/vagrant/.bashrc
+# simplify irb prompt
+echo 'IRB.conf[:PROMPT_MODE] = :SIMPLE' >> /home/vagrant/.irbrc
+chown -R vagrant:vagrant /home/vagrant/.irbrc
+
+sudo apt-get update
+sudo apt-get install -y make
+sudo apt-get install -y git-core expect vim gawk
+sudo apt-get install -y python-software-properties software-properties-common
+
+# install Tmux 2.4
+VERSION=2.4
+sudo apt-get -y remove tmux
+sudo apt-get -y install wget tar libevent-dev libncurses-dev
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-*
+sudo mv tmux-${VERSION} /usr/local/src
+
+# clone a repo used later for tests
+git clone https://github.com/tmux-plugins/tmux-example-plugin /home/vagrant/tmux-example-plugin
+chown -R vagrant:vagrant /home/vagrant/tmux-example-plugin
