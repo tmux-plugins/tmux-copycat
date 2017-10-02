eb3be70ced109596a7ee9e847ae0a7f405f0c361
Thales Mello <thalesmello@gmail.com>
2017-10-02 01:17:39 -0300

    Include helper funciton to verify temux version
    Code snippet was taken from the tmux-yank project.
    https://github.com/tmux-plugins/tmux-yank/

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -182,3 +182,33 @@ _pane_unique_id() {
 	tmux display-message -p "#{session_id}-#{window_index}-#{pane_index}" |
 		sed 's/\$//'
 }
+
+# Cache the TMUX version for speed.
+tmux_version="$(tmux -V | cut -d ' ' -f 2)"
+
+tmux_is_at_least() {
+    if [[ $tmux_version == "$1" || $tmux_version == "master" ]]
+    then
+        return 0
+    fi
+
+    local IFS=.
+    local i tver=($tmux_version) wver=($1)
+
+    # fill empty fields in tver with zeros
+    for ((i=${#tver[@]}; i<${#wver[@]}; i++)); do
+        tver[i]=0
+    done
+
+    # fill empty fields in wver with zeros
+    for ((i=${#wver[@]}; i<${#tver[@]}; i++)); do
+        wver[i]=0
+    done
+
+    for ((i=0; i<${#tver[@]}; i++)); do
+        if ((10#${tver[i]} < 10#${wver[i]})); then
+            return 1
+        fi
+    done
+    return 0
+}
